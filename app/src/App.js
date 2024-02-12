d80cbd64907c6b3e7ffa8902a60ef5b6b61e1640
M4rrs <almasnorazman2803@outlook.com>
2024-02-12 17:37:51 +0800

    functioning generator.
    note: plan to add option to copy password

---
+++ app/src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+	const [state, setState] = useState("");
+
+	const randnum = () => {
+		let randArray = new Uint32Array(1);
+		crypto.getRandomValues(randArray);
+		return (randArray[0]);
+	}
+
+	const cipher = (n, base) => {
+		let max = 126 - n;
+		for (let i = 0; i < base.length; i++) {
+			if (base[i] >= 33 && base[i] <= max)
+				base[i] += n;
+			else if (base[i] >= (max + 1) && base[i] <= 126)
+				base[i] = 33 + (n - (126 - base[i]) - 1);
+		}
+		return (base);
+	}
+
+	const generate = () => {
+		const regex = /^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*\d)(?=.*[A-Z])(?=.*[a-z]).*$/;
+		let len, gen, shifted, pass;
+
+		pass = "";
+		if (crypto && crypto.getRandomValues) {
+			setState("");
+			len = randnum() % 4 + 12;
+			gen = [];
+			for (let i = 0; i < len; i++)
+				gen[i] = randnum() % 94 + 33;
+
+			shifted = cipher(randnum() % 16 + 15, gen);
+			for (let i = 0; i < len; i++)
+				pass += String.fromCharCode(shifted[i]);
+		}
+		regex.test(pass) ? setState(pass) : generate();
+	}
+
+	return (
+		<div id="main">
+			<div id="word">
+				<h1>RanPaGen</h1>
+				<p>Mars' Random Password Generator</p>
+				<p>because I don't trust existing ones.</p>
+				<div id="butt">
+					<button className="button" onClick={generate}>Generate</button>
+				</div>
+				<h3>{state}</h3>
+			</div>
+		</div>
+	);
+}
+
+export default App;
